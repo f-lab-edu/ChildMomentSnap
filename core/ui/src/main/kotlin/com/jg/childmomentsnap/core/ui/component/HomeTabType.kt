68b64339a0413d91ca68ab7c33bf862fe4955fb6
camai <jotolo@naver.com>
2025-10-22 10:41:40 +0900

    feat: BottomTabType #5
    - BottomAppBar Enum 정리
    - Camera icon 추가
    - String Value 수정

--- core/ui/src/main/kotlin/com/jg/childmomentsnap/core/ui/component/HomeTabType.kt
+++ core/ui/src/main/kotlin/com/jg/childmomentsnap/core/ui/component/HomeTabType.kt
@@ -2,8 +2,10 @@ package com.jg.childmomentsnap.core.ui.component
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import com.jg.childmomentsnap.core.ui.R
 
 private const val CALENDAR_PREFIX = "calendar"
+private const val CAMERA_PREFIX = "camera"
 private const val DAILY_PREFIX = "daily"
 private const val MY_PREFIX = "my"
 
@@ -15,4 +17,61 @@ enum class HomeTabType(
     @DrawableRes val unselectedIcon: Int,
     @StringRes val iconTextId: Int
 ) {
+    CALENDAR(
+        prefix = CALENDAR_PREFIX,
+        graph = buildGraph(CALENDAR_PREFIX),
+        route = buildHomeRoute(CALENDAR_PREFIX),
+        selectedIcon = R.drawable.ic_calendar_filled,
+        unselectedIcon = R.drawable.ic_calendar,
+        iconTextId = R.string.bottom_calendar
+    ),
+
+    CAMERA(
+        prefix = CAMERA_PREFIX,
+        graph = buildGraph(CAMERA_PREFIX),
+        route = buildHomeRoute(CAMERA_PREFIX),
+        selectedIcon = R.drawable.ic_camera_filled,
+        unselectedIcon = R.drawable .ic_camera,
+        iconTextId = R.string.bottom_camera
+    ),
+
+    DAILY(
+        prefix = DAILY_PREFIX,
+        graph = buildGraph(DAILY_PREFIX),
+        route = buildHomeRoute(DAILY_PREFIX),
+        selectedIcon = R.drawable.ic_write_filled,
+        unselectedIcon = R.drawable.ic_write,
+        iconTextId = R.string.bottom_daily
+    ),
+
+    MY(
+        prefix = MY_PREFIX,
+        graph = buildGraph(MY_PREFIX),
+        route = buildHomeRoute(MY_PREFIX),
+        selectedIcon = R.drawable.ic_my_filled,
+        unselectedIcon = R.drawable.ic_my,
+        iconTextId = R.string.bottom_my
+    );
+
+    companion object {
+        const val SEPARATOR = "-"
+        const val GRAPH = "graph"
+        const val HOME_ROUTE = "home_route"
+
+        fun HomeTabType.buildRoute(value: String): String = prefix + SEPARATOR + value
+        fun findHome(value: String?): HomeTabType {
+            return if(!value.isNullOrEmpty()) {
+                entries.firstOrNull{ value.startsWith(it.prefix) } ?: CALENDAR
+            } else {
+                CALENDAR
+            }
+        }
+
+        fun isCalendar(value: String?): Boolean {
+            return CALENDAR.route == value
+        }
+    }
 }
+
+private fun buildGraph(prefix: String): String = prefix + HomeTabType.SEPARATOR + HomeTabType.GRAPH
+private fun buildHomeRoute(prefix: String): String = prefix + HomeTabType.SEPARATOR + HomeTabType.HOME_ROUTE
