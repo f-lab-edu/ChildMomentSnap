f5a6044e2d535a0f23ce62f7331129100ad0e0f2
camai <jotolo@naver.com>
2025-12-10 18:51:58 +0900

    fix: HomeGraph xml 대응 코드 제거

--- feature/home/src/main/java/com/jg/childmomentsnap/feature/home/HomeGraph.kt
+++ feature/home/src/main/java/com/jg/childmomentsnap/feature/home/HomeGraph.kt
@@ -16,26 +16,6 @@ import kotlinx.serialization.Serializable
 @Serializable
 data object HomeGraphRoute
 
-/**
- * XML 호환을 위한 상수
- */
-private const val HOME_GRAPH = "home_graph"
-
-fun NavHostController.navigateToHomeGraph(
-    navOptions: NavOptions? = null
-) {
-    this.navigate(HomeGraphRoute, navOptions)
-}
-
-/**
- * XML Navigation으로 Home Graph 이동
- */
-fun NavHostController.navigateToHomeGraphLegacy(
-    navOptions: NavOptions? = null
-) {
-    this.navigate(HOME_GRAPH, navOptions)
-}
-
 
 fun NavGraphBuilder.homeGraph(
     cmsAppState: CmsAppState,
